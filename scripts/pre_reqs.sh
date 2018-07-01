a881c0314e0664ae853cc0cd8e5524ac17b2ee52
Ian Alderman <na>
2018-07-01 18:41:23 +0100

    Re-pushing changes to pre_reqs.sh

--- scripts/pre_reqs.sh
+++ scripts/pre_reqs.sh
@@ -154,7 +154,7 @@ fi
 #Run the deployment
 echo "Starting deployment..."
 
-deploymentOutput=$(az group deployment create -g $rgBaseName --template-uri https://raw.githubusercontent.com/ianalderman/ukopaas/master/azuredeploy.json --parameters $paramString)
+deploymentOutput=$(az group deployment create -g $rgBaseName --template-uri https://raw.githubusercontent.com/ianalderman/ukopaas/master/azuredeploy.json l--parameters $paramString)
 
 echo "Deployment completed..."
 
